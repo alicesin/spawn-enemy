612d7c2dbf471371280e929541c19258ff8bfccd
Alice Sin <sinwinglamalice@gmail.com>
2016-08-11 14:29:22 +0800

    Refine outOfBound logic in EnemyMove

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,7 +5,7 @@ public class EnemyMove : MonoBehaviour {
 
 	private Transform track;
 	[Tooltip("Game Object that the enemies move toward")]
-	public GameObject track_obj;
+	public GameObject trackObj = null;
 
 	[Tooltip("Moving speed of enemy")]
 	public float moveSpeed = 3f;
@@ -13,31 +13,38 @@ public class EnemyMove : MonoBehaviour {
 	[Tooltip("Height that the enemy jump")]
 	public int jumpHeight = 2;
 
-	//public Rigidbody rb;
+	[Tooltip("Boundary of X-cor that an enemy to destroy")]
+	public int minX = -10;
+	[Tooltip("Boundary of X-cor that an enemy to destroy")]
+	public int maxX = 10;
+	[Tooltip("Boundary of Y-cor that an enemy to destroy")]
+	public int minY = -10;
+	[Tooltip("Boundary of Y-cor that an enemy to destroy")]
+	public int maxY = 10;
+	[Tooltip("Boundary of Z-cor that an enemy to destroy")]
+	public int minZ = -10;
+	[Tooltip("Boundary of Z-cor that an enemy to destroy")]
+	public int maxZ = 10; 
 
 	// Use this for initialization
 	void Start () {
-		//rb = GetComponent<Rigidbody> ();
-		track_obj = GameObject.Find("Track");
-		track = track_obj.transform;
+		if (trackObj == null) {
+			trackObj = GameObject.Find("Track");
+		}
+		track = trackObj.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.x < -10 || transform.position.x > 10 || transform.position.y < -10) {
+		bool outOfBound = transform.position.x < minX || transform.position.x > maxX ||
+						  transform.position.y < minY || transform.position.y > maxY ||
+						  transform.position.z < minZ || transform.position.z > maxZ;
+		if (outOfBound) {
 			Destroy(gameObject);
 		} else {
-			float moveHorizontal = Input.GetAxis ("Horizontal");
-			float moveVertical = Input.GetAxis ("Vertical");
-
-			// y = 0.0f, becoz dun want it move up/down
-			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-			//transform.Translate(Vector3.up * 2 * Time.deltaTime, Space.World);
-
 			float move = moveSpeed * Time.deltaTime;
 			transform.Translate(Vector3.up * jumpHeight * Time.deltaTime, Space.World);
 			transform.position = Vector3.MoveTowards(transform.position, track.position, move);
-
 		}
 	}
 }
