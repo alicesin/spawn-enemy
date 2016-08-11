3ea401c1d86fdbb3eaa65d74f7325d6fdc3f470d
Alice Sin <sinwinglamalice@gmail.com>
2016-08-11 14:04:11 +0800

    update spawn.cs

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -10,37 +10,46 @@ public class Spawn : MonoBehaviour {
 	[Tooltip("Current amount of enemier")]
 	public int amount;
 
-	[Tooltip("Max no. of enemies that appear in the same time")]
-	public int maxEnemy;
+	[Tooltip("Max no. of enemies that appear at the same time")]
+	public int maxEnemy = 10;
 
 	[Tooltip("Boundary of X-cor that an enemy appear")]
 	public int minX = -8;
 	[Tooltip("Boundary of X-cor that an enemy appear")]
 	public int maxX = 8;
+	[Tooltip("Boundary of Y-cor that an enemy appear")]
+	public int minY = 0;
+	[Tooltip("Boundary of Y-cor that an enemy appear")]
+	public int maxY = 1;
 	[Tooltip("Boundary of Z-cor that an enemy appear")]
 	public int minZ = -2;
 	[Tooltip("Boundary of Z-cor that an enemy appear")]
 	public int maxZ = 2;
 
-	private Vector3 spawnPoint;
-	
-	// Update is called once per frame
-	void Update () {
-		enemies = GameObject.FindGameObjectsWithTag ("Enemy");
-		amount = enemies.Length;
+	[Tooltip("Min time for new enemy to appear")]
+	public int minSpawnTime = 1;
+	[Tooltip("Max time for new enemy to appear")]
+	public int maxSpawnTime = 5;
 
-		if (amount < maxEnemy) {
-			InvokeRepeating ("spawnEnemy", Random.Range(2, 10), 0f);
-		}
+	[Tooltip("Prefab of the enemy spawn")]
+	public GameObject enemyPrefab;
+
+	private Vector3 spawnPoint;
+
+	void Start () {
+		SpawnEnemy ();
 	}
 
-	void spawnEnemy() {
+	void SpawnEnemy() {
 		spawnPoint.x = Random.Range (minX, maxX);
-		spawnPoint.y = 0.5f;
+		spawnPoint.y = Random.Range (minY, maxY);
 		spawnPoint.z = Random.Range (minZ, maxZ);
 	
-		Instantiate(Resources.Load("Enemy_Prefab"), spawnPoint, Quaternion.identity);
-		CancelInvoke();
-
+		enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		amount = enemies.Length + 1;
+		if (amount < maxEnemy) {
+			Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+		}
+		Invoke ("SpawnEnemy", Random.Range(minSpawnTime, maxSpawnTime));
 	}
 }
