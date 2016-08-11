2e6eb886100cd881f038a096ec6a452630553f92
Alice Sin <sinwinglamalice@gmail.com>
2016-08-11 14:24:26 +0800

    Add tooltip in TrackController

--- Assets/Scripts/TrackController.cs
+++ Assets/Scripts/TrackController.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class TrackController : MonoBehaviour {
 
+	[Tooltip("Object that the track will follow")]
 	public GameObject player;
+
 	// Use this for initialization
 	void Start () {
 	
